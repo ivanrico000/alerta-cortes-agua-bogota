5e3eb000439dad75afbb7b764bef7ad81c63c22d
Ivan Rico <80863069+ivanrico000@users.noreply.github.com>
2025-03-01 13:35:47 -0500

    Se gregan mensajes para la consola en el script de alerta

--- scrapingWeb.py
+++ scrapingWeb.py
@@ -11,16 +11,19 @@ headers = {
 url = "https://bogota.gov.co/"
 response = requests.get(url, headers=headers)
 
+print("✅ Inicio Proceso")
+
 if response.status_code == 200:
     soup = BeautifulSoup(response.text, "html.parser")
     div_element = soup.find(id="reporte1")
 
     if div_element:
         a_element = div_element.find("a")
+        print("✅ Elemento div encontrado")
 
         if a_element:
             enlace = a_element.get("href", "No encontrado")
-            enlace_completo = urljoin(url, enlace)
+            enlace_completo = urljoin(url, enlace)  # Manejo de enlaces relativos
 
             subResponse = requests.get(enlace_completo, headers=headers)
 
@@ -30,19 +33,41 @@ if response.status_code == 200:
 
                 for title in titlesElements:
                     textTitle = title.get_text(strip=True)
+                    print(f"textTitle: {textTitle}")
 
                     if "Racionamiento" in textTitle:
                         match = re.search(r"¡(.*?)!", textTitle)
                         cycle = match.group(1) if match else "No encontrado"
+                        
+                        print(f"Ciclo encontrado: {cycle}")
 
                         strMenssage = ""
                         
                         if cycle.lower() == "turno 3":
+                            print("🔹 Es Turno 3")
                             strMenssage = "Mañana Corte de Agua!"
                         elif cycle.lower() == "turno 4":
+                            print("🔹 Es Turno 4")
                             strMenssage = "HOY Corte de Agua!"
 
+                        # Solo enviar mensaje si hay contenido en strMenssage
                         if strMenssage:
                             subUrl = "https://api.callmebot.com/whatsapp.php?source=web&phone=+573115777165&apikey=6959101&text=" + urllib.parse.quote(strMenssage)
                             responseWhats = requests.get(subUrl, headers=headers)
-                        break 
+
+                            if responseWhats.status_code == 200:
+                                print("✅ Mensaje enviado con éxito")
+                            else:
+                                print(f"⚠️ Error al enviar mensaje. Código: {responseWhats.status_code}")
+
+                        break  # Sale del for una vez encontrado un turno
+                else:
+                    print("⚠️ No se encontró un turno válido, no se enviará mensaje.")
+            else:
+                print(f"⚠️ Error al hacer sub Peticion. Código: {subResponse.status_code}")
+        else:
+            print("⚠️ No se encontró el enlace en el div reporte1")
+    else:
+        print("⚠️ No se encontró el div con la clase 'reporte1'")
+else:
+    print(f"⚠️ Error al obtener la página. Código: {response.status_code}")
