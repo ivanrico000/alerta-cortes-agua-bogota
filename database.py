2b08d7fddf7134146c6a511d176a1ce9b4151e21
Ivan Rico <80863069+ivanrico000@users.noreply.github.com>
2025-03-03 01:41:53 -0500

    Cambio de configuracion para usar Postgrest

--- database.py
+++ database.py
@@ -1,13 +1,14 @@
 import os
 from dotenv import load_dotenv
-import mysql.connector
+import psycopg2
 
 load_dotenv()
 
 def get_database_connection():
-    return mysql.connector.connect(
+    return psycopg2.connect(
         host=os.getenv("DB_HOST"),
         user=os.getenv("DB_USER"),
         password=os.getenv("DB_PASSWORD"),
-        database=os.getenv("DB_NAME")
+        database=os.getenv("DB_NAME"),
+        port=os.getenv("DB_PORT", "5432")
     )
